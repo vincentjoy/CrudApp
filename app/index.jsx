dbd4fbe425aa3739e4879192ecc7b51c92371156
Vincent Joy <vincentjoyhere@gmail.com>
2025-04-22 18:58:53 +0530

    added the theme changer button

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,17 @@
 import { Text, View, TextInput, Pressable, StyleSheet, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import { Inter_500Medium, useFonts } from '@expo-google-fonts/inter/500Medium';
+import Octicons from '@expo/vector-icons/Octicons';
+import { useFonts, Inter_500Medium } from '@expo-google-fonts/inter';
 
 import { data } from "@/data/todos"
 
 export default function Index() {
   const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id)) // Sorting todo's in reverse order
   const [text, setText] = useState('')
+  const { colorScheme, setColorScheme, theme } = useContext(ThemeContext)
 
   const [loaded, error] = useFonts({
     Inter_500Medium,
@@ -17,6 +20,8 @@ export default function Index() {
   if (!loaded && !error) {
     return null
   }
+
+  const styles = createStyles(theme, colorScheme)
 
   const addTodo = () => {
     if (text.trim()) {
@@ -61,6 +66,12 @@ export default function Index() {
         <Pressable onPress={addTodo} style={styles.addButton}>
           <Text style={styles.addButtonText}>Add</Text>
         </Pressable>
+        <Pressable
+          onPress={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')} style={{ marginLeft: 10 }}>
+
+          <Octicons name={colorScheme === 'dark' ? "moon" : "sun"} size={36} color={theme.text} selectable={undefined} style={{ width: 36 }} />
+
+        </Pressable>
       </View>
       <FlatList
         data={todos}
@@ -72,63 +83,65 @@ export default function Index() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-    padding: 10,
-    width: '100%',
-    maxWidth: 1024,
-    marginHorizontal: 'auto',
-    pointerEvents: 'auto',
-  },
-  input: {
-    flex: 1,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    fontSize: 18,
-    fontFamily: 'Inter_500Medium',
-    minWidth: 0,
-    color: 'white',
-  },
-  addButton: {
-    backgroundColor: 'white',
-    borderRadius: 5,
-    padding: 10,
-  },
-  addButtonText: {
-    fontSize: 18,
-    color: 'black',
-  },
-  todoItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between', // Evenly space off children across the container's main axis, distributing the remaining space between the children.
-    gap: 4,
-    padding: 10,
-    borderBottomColor: 'gray',
-    borderBottomWidth: 1,
-    width: '100%',
-    maxWidth: 1024,
-    marginHorizontal: 'auto',
-    pointerEvents: 'auto',
-  },
-  todoText: {
-    flex: 1,
-    fontSize: 18,
-    fontFamily: 'Inter_500Medium',
-    color: 'white',
-  },
-  completedText: {
-    textDecorationLine: 'line-through',
-    color: 'gray',
-  }
-})
+function createStyles(theme, colorScheme) {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.background,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 10,
+      padding: 10,
+      width: '100%',
+      maxWidth: 1024,
+      marginHorizontal: 'auto',
+      pointerEvents: 'auto',
+    },
+    input: {
+      flex: 1,
+      borderColor: 'gray',
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      fontSize: 18,
+      fontFamily: 'Inter_500Medium',
+      minWidth: 0,
+      color: theme.text,
+    },
+    addButton: {
+      backgroundColor: theme.button,
+      borderRadius: 5,
+      padding: 10,
+    },
+    addButtonText: {
+      fontSize: 18,
+      color: colorScheme === 'dark' ? 'black' : 'white',
+    },
+    todoItem: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between', // Evenly space off children across the container's main axis, distributing the remaining space between the children.
+      gap: 4,
+      padding: 10,
+      borderBottomColor: 'gray',
+      borderBottomWidth: 1,
+      width: '100%',
+      maxWidth: 1024,
+      marginHorizontal: 'auto',
+      pointerEvents: 'auto',
+    },
+    todoText: {
+      flex: 1,
+      fontSize: 18,
+      fontFamily: 'Inter_500Medium',
+      color: theme.text,
+    },
+    completedText: {
+      textDecorationLine: 'line-through',
+      color: 'gray',
+    }
+  })
+}
